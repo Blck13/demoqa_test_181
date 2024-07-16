417394a23e4e823915a7677553907a2650b7336a
Blck13 <Dostoyevskiim@mail.ru>
2024-07-16 12:04:16 +0300

    gitignore added right

--- src/test/java/HomeWorkTest/hw/hw5Test2.java
+++ src/test/java/HomeWorkTest/hw/hw5Test2.java
@@ -2,6 +2,7 @@ package HomeWorkTest.hw;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
@@ -15,38 +16,38 @@ public class hw5Test2 extends TestBase {
     private Configuration Configuraion;
 
     @Test
-    public void my2test2(){
+    public void my2test2() {
         Configuraion.pageLoadStrategy = "eager";
         String userName = "Ivan";
         String userLName = "Egorov";
         String userEmail = "krovostok@mail.ru";
         String userGender = "Other";
         String userPhone = "1234567890";
-        String XX= "07";
+        String XX = "07";
         String YY = "July";
         String ZZ = "2008";
         String userSubject = "Chemistry";
         String userHobbie = "Sports";
         String userPicture = "images/3.png";
-        String userAdress ="Svoya ulica kv.5";
+        String userAdress = "Svoya ulica kv.5";
         String userState = "Uttar Pradesh";
         String userCity = "Agra";
 
         registrationPage.openPage()
-                               .setFirstName(userName)
-                               .setLastName(userLName)
-                               .setEmail(userEmail)
-                               .setGender(userGender)
-                               .setPhone(userPhone)
-                               .setBirthDate(XX,YY,ZZ)
-                               .setSubjects(userSubject)
-                               .setHobbies(userHobbie)
-                               .setPicture(userPicture)
-                               .setAdress(userAdress)
-                               .setStateAndCity(userState,userCity);
+                .setFirstName(userName)
+                .setLastName(userLName)
+                .setEmail(userEmail)
+                .setGender(userGender)
+                .setPhone(userPhone)
+                .setBirthDate(XX, YY, ZZ)
+                .setSubjects(userSubject)
+                .setHobbies(userHobbie)
+                .setPicture(userPicture)
+                .setAdress(userAdress)
+                .setStateAndCity(userState, userCity);
 
         registrationPage.verifyResultsModalAppears()
-                .verifyResult("Student Name", "Ivan"+"Egorov")
+                .verifyResult("Student Name", "Ivan" + "Egorov")
                 .verifyResult("Student Email", "krovostok@mail.ru")
                 .verifyResult("Gender", "Other")
                 .verifyResult("Mobile", "1234567890")
